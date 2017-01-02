69057d212c0a8af4d4bc5fb3a44fd2419e77b64a
Tom Randle <tom.randle@gmail.com>
2017-01-02 23:25:12 +0000

    Adding test files

---
+++ sqlite-test.py
@@ -0,0 +1,33 @@
+import sqlite3
+import random
+import time
+
+sqlite_file = 'my_first_db.sqlite'
+table_name = 'readings'
+
+# Connecting to the database file
+
+while True:
+
+	conn = sqlite3.connect(sqlite_file)
+
+	c = conn.cursor()
+
+
+	c.execute("INSERT INTO READINGS (TIME,TEMPERATURE,HUMIDITY,PRESSURE, WINDSPEED, RAINFALL) VALUES (CURRENT_TIMESTAMP, {temp}, 0, 0, 0, 0)".\
+		format(temp=random.randint(1, 100)))
+
+
+	 # c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')".\
+	 #        format(tn=table_name, idf=id_column, cn=column_name))
+
+	time.sleep(10)
+	conn.commit()
+	conn.close()
+
+
+
+# INSERT INTO READINGS (TIME,TEMPERATURE,HUMIDITY,PRESSURE, WINDSPEED, RAINFALL)
+# VALUES (CURRENT_TIMESTAMP, 4, 99, 1000, 0, 0);
+
+
