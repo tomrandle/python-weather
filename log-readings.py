9003cfe664e6262dda3cbe70d38bc541c54522c7
Tom Randle <tom.randle@gmail.com>
2017-01-11 22:37:58 +0000

    Refactored log readings file

--- log-readings.py
+++ log-readings.py
@@ -1,46 +1,26 @@
 import sqlite3
 import time
 
-from Adafruit_BME280 import *
-
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
+##############
+# DHT Sensor #
+##############
 
 import Adafruit_DHT
 
+DHTSensor = Adafruit_DHT.DHT11 #DHT22 when switch sensor
 
-
-
-# Sensor should be set to Adafruit_DHT.DHT11,
-# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
-DHTSensor = Adafruit_DHT.DHT11
-
-# Example using a Beaglebone Black with DHT sensor
-# connected to pin P8_11.
-# pin = 'P8_11'
-
-# Example using a Raspberry Pi with DHT sensor
-# connected to GPIO23.
 DHTPin = 4
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(DHTSensor, DHTPin)
 
 
+#################
+# BME280 Sensor #
+#################
 
-
-
+from Adafruit_BME280 import *
 
 sensor = BME280(mode=BME280_OSAMPLE_8)
-
-sqlite_file = '/home/pi/weatherstation.db'
-table_name = 'readings'
-
-# Connecting to the database file
-
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
 
 # Get sensor readings form BPM208
 
@@ -50,16 +30,19 @@ hectopascals = pascals / 100
 #humidity = sensor.read_humidity()
 
 
+###########
+# MCP3008 #
+###########
 
-
-
-
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
 # Software SPI configuration:
 CLK  = 18
 MISO = 23
 MOSI = 24
 CS   = 25
+
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 windChannel = 7
@@ -69,6 +52,17 @@ rawWindReading = mcp.read_adc(windChannel)
 windspeedMetersPerSecond = rawWindReading
 
 
+###############
+# Write to DB #
+###############
+
+# Connect to the database file
+
+sqlite_file = '/home/pi/weatherstation.db'
+table_name = 'readings'
+
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
 
 
 # Write to db
