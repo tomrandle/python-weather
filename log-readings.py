e685eb87e63e69a9e0b9fde42cec05cb80d2bd8a
Tom Randle <tom.randle@gmail.com>
2017-01-11 22:04:16 +0000

    Add windspeed

--- log-readings.py
+++ log-readings.py
@@ -4,6 +4,11 @@ import time
 
 from Adafruit_BME280 import *
 
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+
+
 sensor = BME280(mode=BME280_OSAMPLE_8)
 
 sqlite_file = '/home/pi/weatherstation.db'
@@ -21,12 +26,34 @@ pascals = sensor.read_pressure()
 hectopascals = pascals / 100
 #humidity = sensor.read_humidity()
 
+
+
+
+
+
+
+# Software SPI configuration:
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+windChannel = 7
+
+rawWindReading = mcp.read_adc(windChannel)
+
+windspeedMetersPerSecond = rawWindReading
+
+
+
+
 # Write to db
 
-c.execute("INSERT INTO READINGS (TIME,TEMPERATURE,HUMIDITY,PRESSURE, WINDSPEED, RAINFALL) VALUES (CURRENT_TIMESTAMP, {temp}, 0, {pressure}, 0, 0)".\
-	format(temp = degrees, pressure = hectopascals))
+c.execute("INSERT INTO READINGS (TIME,TEMPERATURE,HUMIDITY,PRESSURE, WINDSPEED, RAINFALL) VALUES (CURRENT_TIMESTAMP, {temp}, 0, {pressure}, {windspeed}, 0)".\
+	format(temp = degrees, pressure = hectopascals, windspeed = windspeedMetersPerSecond))
 
-
+print windspeedMetersPerSecond
 print "Sensor read"
 
 conn.commit()
