f389cd0812d50c930be2e039cadeb86203395c4d
Tom Randle <tom.randle@gmail.com>
2017-01-13 17:34:26 +0000

    comment out mcp3008 temporarily

--- log-readings.py
+++ log-readings.py
@@ -39,22 +39,22 @@ hectopascals = pascals / 100
 # MCP3008 #
 ###########
 
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
+# import Adafruit_GPIO.SPI as SPI
+# import Adafruit_MCP3008
 
-# Software SPI configuration:
-CLK  = 18
-MISO = 23
-MOSI = 24
-CS   = 25
+# # Software SPI configuration:
+# CLK  = 18
+# MISO = 23
+# MOSI = 24
+# CS   = 25
 
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+# mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
-windChannel = 7
+# windChannel = 7
 
-rawWindReading = mcp.read_adc(windChannel)
+# rawWindReading = mcp.read_adc(windChannel)
 
-windspeedMetersPerSecond = rawWindReading
+# windspeedMetersPerSecond = rawWindReading
 
 ###########
 # One wire Temperature sensor #
