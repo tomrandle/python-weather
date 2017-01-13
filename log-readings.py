54357b97e3ac2322b4067d05d06674b3ff8d41f2
Tom Randle <tom.randle@gmail.com>
2017-01-13 14:20:06 +0000

    fixed file path

--- log-readings.py
+++ log-readings.py
@@ -64,7 +64,7 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 
-temp_sensor = 'sys/bus/w1/devices/28-00000556acc0/w1_slave'
+temp_sensor = '/sys/bus/w1/devices/28-00000556acc0/w1_slave'
 
 def temp_raw():
 	f = open(temp_sensor, 'r')
