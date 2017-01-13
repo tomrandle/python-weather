5913e56956b842d689b4e767fd8066aa1ec6e47a
Tom Randle <tom.randle@gmail.com>
2017-01-13 14:16:47 +0000

    fixed indentation

--- log-readings.py
+++ log-readings.py
@@ -67,26 +67,24 @@ os.system('modprobe w1-therm')
 temp_sensor = 'sys/bus/w1/devices/28-00000556acc0/w1_slave'
 
 def temp_raw():
-
-    f = open(temp_sensor, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
+	f = open(temp_sensor, 'r')
+	lines = f.readlines()
+	f.close()
+	return lines
 
 def read_temp():
 
-    lines = temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = temp_raw()
+	lines = temp_raw()
+	
+	while lines[0].strip()[-3:] != 'YES':
+		time.sleep(0.2)
+		lines = temp_raw()
+		temp_output = lines[1].find('t=')
 
- 		temp_output = lines[1].find('t=')
-
-	    if temp_output != -1:
-	        temp_string = lines[1].strip()[temp_output+2:]
-	        temp_c = float(temp_string) / 1000.0
-	        return temp_c
-
+		if temp_output != -1:
+			temp_string = lines[1].strip()[temp_output+2:]
+			temp_c = float(temp_string) / 1000.0
+			return temp_c
 
 OneWireTemp = read_temp()
 
