05d27c2ba96aeac8bf7eec6baf19082a5f6dacbd
Tom Randle <tom.randle@gmail.com>
2017-01-13 14:14:37 +0000

    fixed quotes

--- log-readings.py
+++ log-readings.py
@@ -63,7 +63,8 @@ windspeedMetersPerSecond = rawWindReading
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-temp_sensor = ‘sys/bus/w1/devices/28-00000556acc0/w1_slave’
+
+temp_sensor = 'sys/bus/w1/devices/28-00000556acc0/w1_slave'
 
 def temp_raw():
 
