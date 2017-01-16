c5068c053b6d30960ee9fb2f3904753aa0e58781
Tom Randle <tom.randle@gmail.com>
2017-01-16 21:33:10 +0000

    Fixed indentation error

--- log-readings.py
+++ log-readings.py
@@ -75,10 +75,10 @@ windspeedMetersPerSecond = rawWindReading
 
 # Convert wind voltage reading to speed (0.4-2V, 0 - 32.4 m/s)
     
-    windspeedMetersPerSecond = ((rawWindReading * 3.3 / 4095) - 0.4) * (32.4 / 1.6);
-    
-    if windspeedMetersPerSecond < 0:
-    	windspeedMetersPerSecond = 0
+windspeedMetersPerSecond = ((rawWindReading * 3.3 / 4095) - 0.4) * (32.4 / 1.6);
+
+if windspeedMetersPerSecond < 0:
+	windspeedMetersPerSecond = 0
 
 
 ###########
