b1268b1e32d9721c970744a0d763d74009c4f617
Tom Randle <tom.randle@gmail.com>
2017-01-16 21:32:15 +0000

    Calculate windspeed

--- log-readings.py
+++ log-readings.py
@@ -72,6 +72,15 @@ print rawWindReading
 
 windspeedMetersPerSecond = rawWindReading
 
+
+# Convert wind voltage reading to speed (0.4-2V, 0 - 32.4 m/s)
+    
+    windspeedMetersPerSecond = ((rawWindReading * 3.3 / 4095) - 0.4) * (32.4 / 1.6);
+    
+    if windspeedMetersPerSecond < 0:
+    	windspeedMetersPerSecond = 0
+
+
 ###########
 # One wire Temperature sensor #
 ###########
