3e7b75a22082ab3ec98758e97752bce7a08a7166
Tom Randle <tom.randle@gmail.com>
2017-01-16 18:27:55 +0000

    Added thingspeak

--- log-readings.py
+++ log-readings.py
@@ -134,3 +134,88 @@ c.execute("INSERT INTO READINGS (TIME,TEMPERATURE1, TEMPERATURE2, TEMPERATURE3,H
 conn.commit()
 conn.close()
 
+
+#######################
+# Write to thingspeak #
+#######################
+
+from __future__ import print_function
+import paho.mqtt.publish as publish
+import psutil
+
+import yaml
+
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+
+###   Start of user configuration   ###   
+
+#  ThingSpeak Channel Settings
+
+# The ThingSpeak Channel ID
+# Replace this with your Channel ID
+channelID = cfg['channelID']
+
+# The Write API Key for the channel
+# Replace this with your Write API key
+apiKey = cfg['apiKey']
+
+#  MQTT Connection Methods
+
+# Set useUnsecuredTCP to True to use the default MQTT port of 1883
+# This type of unsecured MQTT connection uses the least amount of system resources.
+useUnsecuredTCP = True
+
+# Set useUnsecuredWebSockets to True to use MQTT over an unsecured websocket on port 80.
+# Try this if port 1883 is blocked on your network.
+useUnsecuredWebsockets = False
+
+# Set useSSLWebsockets to True to use MQTT over a secure websocket on port 443.
+# This type of connection will use slightly more system resources, but the connection
+# will be secured by SSL.
+useSSLWebsockets = False
+
+###   End of user configuration   ###
+
+# The Hostname of the ThinSpeak MQTT service
+mqttHost = "mqtt.thingspeak.com"
+
+# Set up the connection parameters based on the connection type
+if useUnsecuredTCP:
+    tTransport = "tcp"
+    tPort = 1883
+    tTLS = None
+
+if useUnsecuredWebsockets:
+    tTransport = "websockets"
+    tPort = 80
+    tTLS = None
+
+if useSSLWebsockets:
+    import ssl
+    tTransport = "websockets"
+    tTLS = {'ca_certs':"/etc/ssl/certs/ca-certificates.crt",'tls_version':ssl.PROTOCOL_TLSv1}
+    tPort = 443
+        
+# Create the topic string
+topic = "channels/" + channelID + "/publish/" + apiKey
+
+    
+# build the payload string
+tPayload = "field1=" + str(OneWireTemp) + "&field2=" + str(degrees)
+
+# attempt to publish this data to the topic 
+try:
+    publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
+
+except (KeyboardInterrupt):
+    break
+
+except Exception, e:
+    print ("There was an error while publishing the data." + str(e))
+
+
+
+
+
